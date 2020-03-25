792efd6d20292919be473f98c1c9abb09921f28e
MissEcho <wujrecho@qq.com>
2020-03-26 00:10:29 +0800

    增加其他页面

--- articleeditor/miniprogram/app.js
+++ articleeditor/miniprogram/app.js
@@ -1,7 +1,8 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.globalData = {}
+    let that=this;
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -10,11 +11,9 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        env: 'echo-259cb3',
         traceUser: true,
       })
     }
-
-    this.globalData = {}
   }
 })
---
+++ articleeditor/miniprogram/pages/welcome/welcome.js
@@ -0,0 +1,105 @@
+// miniprogram/pages/welcome/welcome.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res)
+        app.globalData.openid = res.result.openid
+        const db = wx.cloud.database()
+        // 查询当前所有用户
+        db.collection('userTable').where({
+          openId: res.result.openid
+        }).get({
+          success: result => {
+            console.log('[数据库] [查询记录] 成功: ', result)
+            let redirectUrl='../home/home';
+            if(result.data.length>0){
+              redirectUrl=result.data[0].userRole=='teacher'?'../myclass/myclass':'../editing/editing'
+            }
+            setTimeout(function(){
+              wx.reLaunch({
+                url: redirectUrl,
+              })
+            },3000)
+          },
+          fail: err => {
+            wx.showToast({
+              icon: 'none',
+              title: '查询记录失败'
+            })
+            console.error('[数据库] [查询记录] 失败：', err)
+          }
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        // wx.navigateTo({
+        //   url: '../deployFunctions/deployFunctions',
+        // })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
